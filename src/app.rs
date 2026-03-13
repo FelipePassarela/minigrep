062fc25070a4b43039300ba89ce4456e175aaa09
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-03-13 02:42:45 -0300

    feat(search): add case sensitivity option to search function
    Updated the search function to include a case sensitivity parameter, allowing for case-sensitive
    and case-insensitive searches. Adjusted related tests to verify functionality.

--- src/app.rs
+++ src/app.rs
@@ -2,11 +2,19 @@ use crate::config;
 
 use std::{error::Error, fs};
 
-pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+pub fn search<'a>(query: &str, content: &'a str, case_sens: bool) -> Vec<&'a str> {
     let mut result = vec![];
 
+    let query = if case_sens {
+        &query.to_lowercase()
+    } else {
+        query
+    };
+
     for line in content.lines() {
-        if line.contains(query) {
+        let normalized_line = if case_sens { &line.to_lowercase() } else { line };
+
+        if normalized_line.contains(query) {
             result.push(line.trim());
         }
     }
@@ -17,7 +25,7 @@ pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
 pub fn run(config: config::Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &content) {
+    for line in search(&config.query, &content, false) {
         println!("{line}")
     }
 
@@ -38,19 +46,36 @@ They'd banish us, you know.";
     fn one_result() {
         let query = "there";
         let result = vec!["Then there's a pair of us - don't tell!"];
-        assert_eq!(result, search(query, POEM));
+        assert_eq!(result, search(query, POEM, false));
+    }
+
+    #[test]
+    fn multiple_results() {
+        let query = "no";
+        let result = vec![
+            "I'm nobody! Who are you?",
+            "Are you nobody, too?",
+            "They'd banish us, you know.",
+        ];
+        assert_eq!(result, search(query, POEM, false));
     }
 
     #[test]
     fn no_result() {
         let query = "monophormization";
-        assert!(search(query, POEM).is_empty());
+        assert!(search(query, POEM, false).is_empty());
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "aRe";
+        assert!(search(query, POEM, false).is_empty());
     }
 
     #[test]
     fn case_insensitive() {
         let query = "aRe";
         let result = vec!["I'm nobody! Who are you?", "Are you nobody, too?"];
-        assert_eq!(result, search(query, POEM));
+        assert_eq!(result, search(query, POEM, true));
     }
 }
