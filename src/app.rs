14fbbd3f3f449e3a3d68acc6a64f05b5aff0d1f4
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2026-03-13 03:18:43 -0300

    feat(search): implement case sensitivity in search function
    Added a Case enum and SearchParams struct to handle case sensitivity options in the search function.
    Updated the search logic and tests to accommodate both case-sensitive and case-insensitive searches.

--- src/app.rs
+++ src/app.rs
@@ -2,19 +2,35 @@ use crate::config;
 
 use std::{error::Error, fs};
 
-pub fn search<'a>(query: &str, content: &'a str, case_sens: bool) -> Vec<&'a str> {
+#[derive(Debug, PartialEq)]
+pub enum Case {
+    Sensitive,
+    Insensitive,
+}
+
+#[derive(Debug)]
+pub struct SearchParams {
+    pub case: Case,
+}
+
+pub fn search<'a>(query: &str, content: &'a str, params: SearchParams) -> Vec<&'a str> {
     let mut result = vec![];
 
-    let query = if case_sens {
+    let case_sens = params.case == Case::Insensitive;
+    let normalized_query = if case_sens {
         &query.to_lowercase()
     } else {
         query
     };
 
     for line in content.lines() {
-        let normalized_line = if case_sens { &line.to_lowercase() } else { line };
+        let normalized_line = if case_sens {
+            &line.to_lowercase()
+        } else {
+            line
+        };
 
-        if normalized_line.contains(query) {
+        if normalized_line.contains(normalized_query) {
             result.push(line.trim());
         }
     }
@@ -25,7 +41,10 @@ pub fn search<'a>(query: &str, content: &'a str, case_sens: bool) -> Vec<&'a str
 pub fn run(config: config::Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &content, false) {
+    let params = SearchParams {
+        case: Case::Sensitive,
+    };
+    for line in search(&config.query, &content, params) {
         println!("{line}")
     }
 
@@ -45,37 +64,52 @@ They'd banish us, you know.";
     #[test]
     fn one_result() {
         let query = "there";
+        let params = SearchParams {
+            case: Case::Insensitive,
+        };
         let result = vec!["Then there's a pair of us - don't tell!"];
-        assert_eq!(result, search(query, POEM, false));
+        assert_eq!(result, search(query, POEM, params));
     }
 
     #[test]
     fn multiple_results() {
         let query = "no";
+        let params = SearchParams {
+            case: Case::Insensitive,
+        };
         let result = vec![
             "I'm nobody! Who are you?",
             "Are you nobody, too?",
             "They'd banish us, you know.",
         ];
-        assert_eq!(result, search(query, POEM, false));
+        assert_eq!(result, search(query, POEM, params));
     }
 
     #[test]
     fn no_result() {
         let query = "monophormization";
-        assert!(search(query, POEM, false).is_empty());
+        let params = SearchParams {
+            case: Case::Insensitive,
+        };
+        assert!(search(query, POEM, params).is_empty());
     }
 
     #[test]
     fn case_sensitive() {
         let query = "aRe";
-        assert!(search(query, POEM, false).is_empty());
+        let params = SearchParams {
+            case: Case::Sensitive,
+        };
+        assert!(search(query, POEM, params).is_empty());
     }
 
     #[test]
     fn case_insensitive() {
         let query = "aRe";
+        let params = SearchParams {
+            case: Case::Insensitive,
+        };
         let result = vec!["I'm nobody! Who are you?", "Are you nobody, too?"];
-        assert_eq!(result, search(query, POEM, true));
+        assert_eq!(result, search(query, POEM, params));
     }
 }
